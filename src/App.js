00cb4b6c3b91c0e53885663e12166eab86201ec4
swasell <svenwasell@gmail.com>
2022-06-15 20:45:27 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard 
+        firstName= { 'Jane' } lastName= { 'Doe' }
+        age= { 45 }
+        hairColor= { 'Black' } 
+      />
+      <PersonCard 
+        firstName= { 'John' } lastName= { 'Smith' }
+        age= { 29 }
+        hairColor= { 'Blonde' } 
+      />
+      <PersonCard 
+        firstName= { 'Maria' } lastName= { 'Smith' }
+        age= { 100 }
+        hairColor= { 'Brunette' } 
+      />
     </div>
   );
 }
